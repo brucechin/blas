ec8fd418773c7bc215f45ea77ca11c539157ce0c
Lianke Qin <1026193951@sjtu.edu.cn>
2019-08-13 15:22:02 +0800

    update MatrixPool.java

--- java/MatrixPool.java
+++ java/MatrixPool.java
@@ -7,83 +7,102 @@ import blas.java.*;
 import java.lang.*;
 
 public class MatrixPool {
-    private int _size;
+    private int _minPoolSize;
+    private int _maxPoolSize;
     private int _nrow;
     private int _ncol;
     private boolean _isOn;
-    private LinkedList<PooledMatrix> _freeMatrixList;
-    private LinkedList<PooledMatrix> _usedMatrixList;
+    private LinkedList<Matrix> _freeMatrixList;
+    private LinkedList<Matrix> _usedMatrixList;
     final Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
         int nrow = 2000;
         int ncol = 2000;
-        int poolsize = 50;
-        MatrixPool pool = new MatrixPool(poolsize, nrow, ncol);
-        for (int i = 0; i < poolsize; i++) {
+        int minsize = 10;
+        int maxsize = 30;
+        MatrixPool pool = new MatrixPool(minsize, maxsize, nrow, ncol);
+        for (int i = 0; i < maxsize; i++) {
             new Thread(new Consumer(pool, nrow, ncol), "Thread " + String.valueOf(i)).start();
         }
 
     }
 
-    public MatrixPool(int poolsize, int nrow, int ncol) {
-        _size = poolsize;
-        _freeMatrixList = new LinkedList<PooledMatrix>();
-        _usedMatrixList = new LinkedList<PooledMatrix>();
+    public MatrixPool(int minsize, int maxsize, int nrow, int ncol) {
+        _minPoolSize = minsize;
+        _maxPoolSize = maxsize;
+        _freeMatrixList = new LinkedList<Matrix>();
+        _usedMatrixList = new LinkedList<Matrix>();
         _ncol = ncol;
         _nrow = nrow;
         _isOn = true;
-        for (int i = 0; i < _size; i++) {
+        for (int i = 0; i < _minPoolSize; i++) {
             PooledMatrix mat = new PooledMatrix(nrow, ncol);
             _freeMatrixList.add(mat);
         }
 
     }
 
-    public PooledMatrix get(int nrow, int ncol) {
-        PooledMatrix res;
+    public Matrix get(int nrow, int ncol) {
+        Matrix res = null;
         lock.lock();
         try {
-            if (_isOn) {
-                if (_freeMatrixList.size() > 0 && _ncol == ncol && _nrow == nrow) {
+            if (_isOn && _ncol == ncol && _nrow == nrow) {
+                if (_freeMatrixList.size() > 0) {
                     res = _freeMatrixList.getFirst();
                     _freeMatrixList.remove();
                     _usedMatrixList.add(res);
-                } else {
-                    System.out.println("allocating extra memory");
+
+                    System.out.println(
+                            Thread.currentThread().getName() + " --get-- free matrix num : " + _freeMatrixList.size());
+                } else if (_freeMatrixList.size() + _usedMatrixList.size() < _maxPoolSize) {
                     res = new PooledMatrix(nrow, ncol);
+                    _usedMatrixList.add(res);
+
+                    System.out.println(Thread.currentThread().getName() + " --get-- increase pool size ");
                 }
-                System.out.println(
-                        Thread.currentThread().getName() + " --get-- free matrix num : " + _freeMatrixList.size());
-                return res;
-            } else {
-                // what shall we return in get() after the pool is down
-                res = new PooledMatrix();
-                return res;
+
             }
 
         } finally {
             lock.unlock();
         }
+        if (res == null) {
+            res = new Matrix(nrow, ncol);
+            System.out.println(
+                    Thread.currentThread().getName() + " --get-- empty free PooledMatrixList, allocate Matrix instead");
+        }
+
+        return res;
 
     }
 
-    public boolean release(PooledMatrix mat) {
+    public boolean release(Matrix mat) {
         lock.lock();
         try {
-            if (_usedMatrixList.contains(mat)) {
+            if (_ncol == mat.getNCol() && _nrow == mat.getNRow()) {
                 _usedMatrixList.remove(mat);
                 _freeMatrixList.add(mat);
                 System.out.println(
                         Thread.currentThread().getName() + " --release-- free matrix num : " + _freeMatrixList.size());
                 return true;
-            } else if (!_freeMatrixList.contains(mat) && mat.getNCol() == _ncol && mat.getNRow() == _nrow) {
-                _freeMatrixList.add(mat);
-                _size++;
-                System.out.println(
-                        Thread.currentThread().getName() + " --release-- free matrix num : " + _freeMatrixList.size());
-                return true;
             }
+
+            // if (_usedMatrixList.contains(mat)) {
+            // _usedMatrixList.remove(mat);
+            // _freeMatrixList.add(mat);
+            // System.out.println(
+            // Thread.currentThread().getName() + " --release-- free matrix num : " +
+            // _freeMatrixList.size());
+            // return true;
+            // } else if (!_freeMatrixList.contains(mat) && mat.getNCol() == _ncol &&
+            // mat.getNRow() == _nrow) {
+            // _freeMatrixList.add(mat);
+            // System.out.println(
+            // Thread.currentThread().getName() + " --release-- free matrix num : " +
+            // _freeMatrixList.size());
+            // return true;
+            // }
 
         } finally {
             lock.unlock();
@@ -98,11 +117,12 @@ public class MatrixPool {
     // return true;
     // }
 
-    public boolean shutDown() throws InterruptedException {
+    public boolean clear() throws InterruptedException {
         _isOn = false;
         _freeMatrixList.clear();
         _usedMatrixList.clear();
-        _size = 0;
+        _minPoolSize = 0;
+        _maxPoolSize = 0;
         System.out.println("Matrix pool shuts down completely");
         return true;
     }
