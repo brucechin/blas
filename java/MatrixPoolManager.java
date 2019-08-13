2629c905b5c8a49a7c9a25d68033a114787097cd
Lianke Qin <1026193951@sjtu.edu.cn>
2019-08-13 15:22:46 +0800

    add Matrix Pool Manager, singleton pattern

---
+++ java/MatrixPoolManager.java
@@ -0,0 +1,54 @@
+package blas.java;
+
+import java.util.*;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.*;
+import blas.java.*;
+import java.lang.*;
+
+public class MatrixPoolManager {
+
+    // fixed min/max pool size
+    private int _minPoolSize = 10;
+    private int _maxPoolSize = 30;
+    private ConcurrentHashMap<String, MatrixPool> _poolMap;// key is a string "nrow+ncol"
+
+    private static MatrixPoolManager manager;
+
+    public static void main(String[] args) {
+        MatrixPoolManager m = MatrixPoolManager.getMatrixPoolManager();
+        Matrix res = m.get(100, 100);
+    }
+
+    public Matrix get(int nrow, int ncol) {
+        Matrix res = null;
+        String key = String.valueOf(nrow) + "+" + String.valueOf(ncol);
+
+        if (!poolMap.containsKey(key)) {
+            // allocate new matrix pool for this (nrow, ncol)
+            poolMap.put(key, new MatrixPool(_minPoolSize, _maxPoolSize, nrow, ncol));
+        }
+
+        res = poolMap.get(key).get(nrow, ncol);
+
+        return res;
+    }
+
+    public boolean release(Matrix mat) {
+        String key = String.valueOf(mat.getNRow()) + "+" + String.valueOf(mat.getNCol());
+        _poolMap.get(key).release(mat);
+        return false;
+    }
+
+    private MatrixPoolManager() {
+        _poolMap = new ConcurrentHashMap<String, MatrixPool>();
+    }
+
+    public static synchronized MatrixPoolManager getMatrixPoolManager() {
+        if (manager == null) {
+            manager = new MatrixPoolManager();
+        }
+        return manager;
+    }
+
+}
